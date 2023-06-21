f3bdec3360fc633a951e60143b50937cc6480d7b
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2023-06-21 11:12:41 -0300

    add task struct

---
+++ models/models.go
@@ -0,0 +1,9 @@
+package models
+
+import "gorm.io/gorm"
+
+type Task struct {
+	gorm.Model
+	Description string `json:"description"` gorm:"text; not null; default:null"
+	Completed  bool `json:"completed"` gorm:"not null; default:false"
+}
