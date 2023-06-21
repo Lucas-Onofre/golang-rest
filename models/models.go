9537a11705ffeef825e41c1a987ccb275b791c86
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2023-06-21 11:37:15 -0300

    config database connection

--- models/models.go
+++ models/models.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Task struct {
 	gorm.Model
-	Description string `json:"description"` gorm:"text; not null; default:null"
-	Completed  bool `json:"completed"` gorm:"not null; default:false"
+	Description string `json:"description" gorm:"text; not null; default:null`
+	Completed  bool `json:"completed" gorm:"not null; default:false`
 }
